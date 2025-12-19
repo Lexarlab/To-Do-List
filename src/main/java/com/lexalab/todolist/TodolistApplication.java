0a51fb5965bda614bc9d1b49a285e19f2c2e00c2
Lexarlab <alexiabezerra.c@gmail.com>
2025-12-19 15:29:00 +1100

    first commit

---
+++ src/main/java/com/lexalab/todolist/TodolistApplication.java
@@ -0,0 +1,16 @@
+package com.lexalab.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class
+})
+public class TodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistApplication.class, args);
+	}
+
+}
