a355fb7b800abeaaebc7413a865d96d34425bcd3
Lexarlab <alexiabezerra.c@gmail.com>
2025-12-20 14:52:06 +1100

    Finish Executable

--- src/main/java/com/lexalab/todolist/service/TodoService.java
+++ src/main/java/com/lexalab/todolist/service/TodoService.java
@@ -2,6 +2,7 @@ package com.lexalab.todolist.service;
 
 import com.lexalab.todolist.entity.Todo;
 import com.lexalab.todolist.repository.TodoRepository;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,16 +14,25 @@ public class TodoService {
     public TodoService(TodoRepository todoRepository) {
         this.todoRepository = todoRepository;
     }
+
     public List<Todo> create(Todo todo) {
         todoRepository.save(todo);
+        return list();
     }
 
     public List<Todo> list(){
-        Sort.by()
-        todoRepository.findAll();
+        Sort sort = Sort.by(Sort.Direction.DESC, "priority")
+                .and(Sort.by(Sort.Direction.ASC, "name"));
+        return todoRepository.findAll(sort);
     }
 
-    public List<Todo> update() {}
+    public List<Todo> update(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
 
-    public List<Todo> delete() {}
+    public List<Todo> delete(Long id) {
+        todoRepository.deleteById(id);
+        return list();
+    }
 }
